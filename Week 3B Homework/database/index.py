003c656786c39ddb3239c2a8972590e0d891dfd1
PatryKing16 <P.krakowiak36@gmail.com>
2024-09-05 10:03:35 +0100

    new

--- Week 3B Homework/database/index.py
+++ Week 3B Homework/database/index.py
@@ -39,12 +39,17 @@ import sqlite3
 con.execute("""SELECT ename, name FROM customer c INNER JOIN emp e ON c.repid = e.empno ORDER BY ename""")
 
 all_rows = con.fetchall()
+last_repname = ""
+
 
 print("Sales Rep \t Customers name")
 
 for row in all_rows:
     repname = row[0]
     custname = row[1]
-    print(f"{repname}\t{custname}")
-
+    if repname != last_repname:
+        print("{0}\t {1}".format(repname, custname))
+        lastname = repname
+    else:
+        print("{0}\t {1}".format("", custname))
 db.close()

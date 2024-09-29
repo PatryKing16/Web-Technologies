e26165da80d2f66390eb03f81b8b41f9a0b57acf
PatryKing16 <P.krakowiak36@gmail.com>
2024-09-29 13:17:33 +0100

    ver0.8

--- Project/10429636/index.js
+++ Project/10429636/index.js
@@ -2,26 +2,42 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const app = express();
-const db = new sqlite3.Database('./database/theme_park.db'); 
+
+// Connect to the SQLite database
+const db = new sqlite3.Database('./database/theme_park.db', (err) => {
+    if (err) {
+        console.error("Error opening database:", err.message);
+    } else {
+        console.log("Connected to the SQLite database.");
+    }
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+
+// Home route
 app.get('/', (req, res) => {
     res.render('index', { title: 'Welcome to the Theme Park!' });
 });
+
+// Areas route
 app.get('/areas', (req, res) => {
     db.all('SELECT * FROM areas', [], (err, rows) => {
         if (err) throw err;
         res.render('areas', { areas: rows });
     });
 });
+
+// Attractions route
 app.get('/attractions', (req, res) => {
     db.all('SELECT * FROM attractions', [], (err, rows) => {
         if (err) throw err;
         res.render('attractions', { attractions: rows });
     });
 });
+
+// Attractions by Area ID route
 app.get('/attractions/:area_id', (req, res) => {
     const areaId = req.params.area_id;
     db.all('SELECT * FROM attractions WHERE area_id = ?', [areaId], (err, rows) => {
@@ -29,17 +45,34 @@ app.get('/attractions/:area_id', (req, res) => {
         res.render('attractions', { attractions: rows });
     });
 });
+
+// FAQ route
 app.get('/faq', (req, res) => {
     res.render('faq');
 });
+
+// Contact page route (GET)
 app.get('/contact', (req, res) => {
     res.render('contact');
 });
+
 app.post('/contact', (req, res) => {
-    // Process form data here 
-    res.send('Thank you for contacting us!');
+    const { name, email, message } = req.body;
+
+    const query = `INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)`;
+    db.run(query, [name, email, message], function(err) {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send("There was an error processing your request.");
+        } else {
+            // Render the contact page with a flag indicating success
+            res.render('contact', { showThankYou: true });
+        }
+    });
 });
 
+
+// Start the server
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
